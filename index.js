7e3c1e1e9c5b94116b95cfe71eb280d8b86affb3
Nicole Park <nicolepark@bellsouth.net>
2023-10-05 14:35:04 -0400

    changes to psudocode and alert

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 /* Pseudocode
     1. create a variable for the user message
-    2. create three seperate variables for each of the combination numbers, using the different math operations
-    3. create an alert message concatenating the four variables created together.*/
+    2. create variable 1, giving it the value of 10 using math operation multiplication
+    3. create variable 2, giving it the value of 40 using math operations addition/subtraction
+    4. create variable 3, giving it the value of 39 using math operations division/addition
+    5. create an alert message 
+    6. In the alert message conctenate the four variables
+    */
 
 // assigning the user message a variable
 const vaultMsg = "You have received this message because yo have been chosen to open an important vault.  Here is the secret combination:";
@@ -13,4 +17,5 @@ let code2 = (8 + 2) * 5 - 10;
 let code3 = 100 / 4 + 14;
 
 // creating an alert message to display in the browser
-alert(vaultMsg + " " + code1 + "-" + code2 + "-" + code3);
+// alert(vaultMsg + " " + code1 + "-" + code2 + "-" + code3);
+alert(`${vaultMsg} ${code1}-${code2}-${code3}`);
